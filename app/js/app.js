2c33fa0cdd8475ed77c47b3d3e641645f710fd18
Vincent Daubry <vdaubry@gmail.com>
2014-08-08 20:54:35 +0200

    ajout template angular

---
+++ app/js/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+var beerSearchApp = angular.module('beerSearchApp', [
+  'ngRoute',
+  'beerSearchApp.filters',
+  'beerSearchApp.services',
+  'beerSearchApp.directives',
+  'beerSearchApp.controllers'
+]);
+
+
+beerSearchApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeController'});
+  $routeProvider.when('/beers', {templateUrl: 'partials/beers.html', controller: 'BeersController'});
+  $routeProvider.otherwise({redirectTo: '/home'});
+}]);
---
+++ app/js/controllers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Controllers */
+
+var beerSearchControllers = angular.module('beerSearchApp.controllers', [])
+
+beerSearchControllers.controller('HomeController', ['$scope', function($scope) {
+}])
+
+beerSearchControllers.controller('BeersController', ['$scope', function($scope) {
+}]);
